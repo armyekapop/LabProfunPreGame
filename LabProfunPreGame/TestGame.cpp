d2c52522138587485831abab906d7bd82a9c14c7
EKAPOP ONSAMANG <63011090@KMITL.AC.TH>
2020-10-04 20:22:54 +0700

    เพิ่มยิงได้

--- LabProfunPreGame/TestGame.cpp
+++ LabProfunPreGame/TestGame.cpp
@@ -1,214 +1,110 @@
 #include<stdio.h>
-#include<Windows.h>
+#include<windows.h>
 #include<conio.h>
-void draw_ship(int, int);
-void erase_ship(int, int);
 void setcursor(bool);
 void setcolor(int, int);
-void move();
-void shoot(int, int);
-void scr_bullet(int);
-int main()
+void gotoxy(int, int);
+void erase_ship(int, int);
+void draw_ship(int, int);
+int main() 
 {
-	system("COLOR E3");
-	setcursor(0);
-	char ch = ' ';
-	int x = 38, y = 20;
-	int x_shoot = x+3, y_shoot = y - 1;
-	int bullet = 5;
-	setcolor(2, 4);
-	draw_ship(x, y);
-	do
-	{
-		setcolor(2, 4);
-		scr_bullet(bullet);
-		if (_kbhit())
-		{
-			ch = _getch();
-			if (ch == 'a' and x > 0)
-			{
-				while (x > 0)
-				{
-					if (_kbhit())
-					{
-						ch = _getch();
-						if (ch == 's')
-						{
-							ch = ' ';
-							break;
-						}
-					}
-					setcolor(14, 14);
-					erase_ship(x, y);
-					setcolor(2, 4);
-					draw_ship(--x, y);
-					Sleep(70);
-				}
-			}
-			if (ch == 'd' and x < 80)
-			{
-				while (x < 80)
-				{
-					if (_kbhit())
-					{
-						ch = _getch();
-						if (ch == 's')
-						{
-							ch = ' ';
-							break;
-						}
-					}
-					setcolor(14, 14);
-					erase_ship(x, y);
-					setcolor(2, 4);
-					draw_ship(++x, y);
-					Sleep(70);
-				}
-			}
-			if (ch == 'w' and y > 0)
-			{
-				setcolor(14, 14);
-				erase_ship(x, y);
-				setcolor(2, 4);
-				draw_ship(x, --y);
-			}
-			if (ch == 's' and y < 24)
-			{
-				setcolor(14, 14);
-				erase_ship(x, y);
-				setcolor(2, 4);
-				draw_ship(x, ++y);
-			}
-			y_shoot = y - 1;
-			x_shoot = x + 3;
-			if (ch == 'h' && bullet > 0)
-			{
-				bullet--;
-				scr_bullet(bullet);
-				while (y_shoot > 0)
-				{
-					setcolor(14, 14);
-					erase_ship(x_shoot, y_shoot);
-					setcolor(0, 14);
-					shoot(x_shoot, --y_shoot);
-					Sleep(100);
-					if (y_shoot == 0)
-					{
-						setcolor(14, 14);
-						erase_ship(x_shoot, 0);
-					}
-				}
-			}
-			fflush(stdin);
-		}
-		Sleep(70);
-	} while (ch != 'x');
-	return 0;
-}
-void move()
-{
-	char ch = ' ';
-	int x = 38, y = 20;
-	setcolor(2, 4);
-	draw_ship(x, y);
-	do
-	{
-		if (_kbhit())
-		{
-			ch = _getch();
-			if (ch == 'a' and x > 0)
-			{
-				while (x > 0)
-				{
-					if (_kbhit())
-					{
-						ch = _getch();
-						if (ch == 's')
-						{
-							ch = ' ';
-							break;
-						}
-					}
-					setcolor(14, 14);
-					erase_ship(x, y);
-					setcolor(2, 4);
-					draw_ship(--x, y);
-					Sleep(70);
-				}
-			}
-			if (ch == 'd' and x < 80)
-			{
-				while (x < 80)
-				{
-					if (_kbhit())
-					{
-						ch = _getch();
-						if (ch == 's')
-						{
-							ch = ' ';
-							break;
-						}
-					}
-					setcolor(14, 14);
-					erase_ship(x, y);
-					setcolor(2, 4);
-					draw_ship(++x, y);
-					Sleep(70);
-				}
-			}
-			if (ch == 'w' and y > 0)
-			{
-				setcolor(14, 14);
-				erase_ship(x, y);
-				setcolor(2, 4);
-				draw_ship(x, --y);
-			}
-			if (ch == 's' and y < 24)
-			{
-				setcolor(14, 14);
-				erase_ship(x, y);
-				setcolor(2, 4);
-				draw_ship(x, ++y);
-			}
-			fflush(stdin);
-		}
-		Sleep(70);
-	} while (ch != 'x');
-}
-void draw_ship(int x, int y)
-{
-	COORD c = { x,y };
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-	printf(" <-0-> ");
-}
-void erase_ship(int x, int y)
-{
-	COORD c = { x,y };
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-	printf("       ");
-}
-void setcursor(bool visible)
-{
-	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
-	CONSOLE_CURSOR_INFO lpCursor;
-	lpCursor.bVisible = visible;
-	lpCursor.dwSize = 20;
-	SetConsoleCursorInfo(console, &lpCursor);
+    setcursor(0);
+    char ch = ' ';
+    int x = 38, y = 20;
+    draw_ship(x, y);
+    int direction_x = 0;
+    int direction_y = 0;
+    bool bulletStatus = false;
+    int xbullet, ybullet, maxbullet = 5;
+    do {
+        if (_kbhit())
+        {
+            ch = _getch();
+            if (ch == 'a')
+            {
+                direction_x = -1;
+            }
+            if (ch == 'd')
+            {
+                direction_x = 1;
+            }
+            if (ch == 's')
+            {
+                if (direction_x == 0)
+                {
+                    direction_y = 1;
+                }
+                direction_x = 0;
+            }
+            if (ch == 'w')
+            {
+                if (direction_x == 0)
+                {
+                    direction_y = -1;
+                }
+            }
+            if (ch == ' ' && !bulletStatus) {
+                bulletStatus = true;
+                xbullet = x + 2, ybullet = y;
+                maxbullet--;
+            }
+            fflush(stdin);
+        }
+        if (bulletStatus && maxbullet >= 0) 
+        {
+            gotoxy(xbullet, (ybullet != y) ? ybullet : ybullet - 1);
+            setcolor(0, 0);
+            printf(" ");
+            if (ybullet > 0) {
+                gotoxy(xbullet, --ybullet);
+                setcolor(6, 0);
+                printf("*");
+            }
+            else bulletStatus = false;
+        }
+        if (direction_x != 0 && x + direction_x >= 0 && x + direction_x <= 80)
+        {
+            erase_ship(x, y);
+            draw_ship(x += direction_x, y);
+        }
+        if (direction_y != 0 && y + direction_y >= 0 && y + direction_y <= 24)
+        {
+            erase_ship(x, y);
+            draw_ship(x += direction_x, y += direction_y);
+            direction_y = 0;
+        }
+        Sleep(100);
+    } while (ch != 'x');
+    setcolor(7, 0);
+    return 0;
 }
 void setcolor(int fg, int bg)
 {
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, bg * 16 + fg);
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, bg * 16 + fg);
 }
-void shoot(int x, int y)
+void gotoxy(int x, int y)
 {
-	COORD c = { x,y };
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-	printf("*");
+    COORD c = { x,y };
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
-void scr_bullet(int bullet)
+void erase_ship(int x, int y)
 {
-	COORD c = { 0,0 };
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-	printf("BULLET : %d", bullet);
+    gotoxy(x, y);
+    setcolor(0, 0);
+    printf("     ");
+}
+void draw_ship(int x, int y)
+{
+    gotoxy(x, y);
+    setcolor(2, 4);
+    printf("<-0->");
+}
+void setcursor(bool visible)
+{
+    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO lpCursor;
+    lpCursor.bVisible = visible;
+    lpCursor.dwSize = 20;
+    SetConsoleCursorInfo(console, &lpCursor);
 }
